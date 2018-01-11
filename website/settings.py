f13cab9e78e2cb0a9de2ed49380f2a637e1d396a
Tymo <Tymo>
2018-01-11 17:14:15 +0100

    error fixing

--- website/settings.py
+++ website/settings.py
@@ -118,15 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-#STATIC_URL = '/static/'
-#MEDIA_ROOT = 'schoolproject/static'
+STATIC_URL = '/static/'
+MEDIA_ROOT = 'schoolproject/static'
 
 
-#STATICFILES_DIRS = (
-#  os.path.join(BASE_DIR, 'schoolproject/static'),
-#)
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'schoolproject/static'),
+)
 
-STATIC_URL ='/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
