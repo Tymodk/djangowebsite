9589d6b787f9c31501c8e6de3dd0848ce1c70832
Tymo <Tymo>
2018-01-11 17:00:41 +0100

    error fixing

--- website/settings.py
+++ website/settings.py
@@ -119,10 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_ROOT = 'schoolproject/static'
-STATIC_ROOT = '/home/Tymodekock/djangowebsite/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'schoolproject/static'),
 )
